6f59df447a0afc3ce7fa29947f829e8e3de34742
Q00 <jqyu.lee@gmail.com>
2020-07-26 18:41:01 +0900

    feat: ⚡️ add user, shared to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './services/app.service';
 import { AppController } from './controllers/app.controller';
 import { PrismaService } from './services/prisma.service';
+import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, SharedModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
